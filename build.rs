0592f933196e01bf37d6d2d229c2facc73ae19b1
CaleHH <caleihhmy@gmail.com>
2021-06-03 19:40:20 +0800

    add chat message proto

--- build.rs
+++ build.rs
@@ -4,4 +4,5 @@ fn main() {
     prost_build::compile_protos(&["src/relay.proto"], &["src"]).unwrap();
     prost_build::compile_protos(&["src/payload.proto"], &["src"]).unwrap();
     prost_build::compile_protos(&["src/gossip.proto"], &["src"]).unwrap();
+    prost_build::compile_protos(&["src/chat.proto"], &["src"]).unwrap();
 }
